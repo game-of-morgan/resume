a12d4295a06d45df3cbdf69250ce9d51a99bc7b7
Morgan Wowk <morgan.wowk+github@gmail.com>
2020-12-08 01:19:10 -0600

    Terminal list functionality

--- src/components/Typewriter/StaticTypewriter.js
+++ src/components/Typewriter/StaticTypewriter.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Typewriter.module.css';
+import { useKeyPress } from '../../helpers/hooks';
 
 const BREAKPOINT_PATTERN = /-.-/g;
 
@@ -7,28 +8,57 @@ const Cursor = ({ visible }) => (
   <span className={!visible && styles.HideCursor}>_</span>
 );
 
-const StaticTypewriter = ({ text }) => {
-  const [output, setOutput] = useState('');
+const StaticTypewriter = ({ lines }) => {
+  const [output, setOutput] = useState([]);
   const [showCursor, setShowCursor] = useState(true);
+  const [currentLineIndex, setCurrentLineIndex] = useState(0);
+  const enterKeyPress = useKeyPress('enter');
+
+  useEffect(() => {
+    if (
+      enterKeyPress
+      && lines[currentLineIndex + 1]
+    ) {
+      if (output[currentLineIndex].length < lines[currentLineIndex].length) {
+        setOutput((prevOutput) => {
+          const newOutput = [...prevOutput];
+          newOutput[currentLineIndex] = lines[currentLineIndex];
+          return newOutput;
+        });
+      }
+      setCurrentLineIndex(currentLineIndex + 1);
+    }
+  }, [enterKeyPress]);
 
   // Typewrite
   useEffect(() => {
-    if (output !== text) {
-      const breakpoint = text.substr(output.length, '---'.length);
+    const lineText = lines[currentLineIndex];
+    const lineOutput = output[currentLineIndex] || [];
+    if (lineOutput !== lineText) {
+      const breakpoint = lineText.substr(lineOutput.length, '---'.length);
       if (BREAKPOINT_PATTERN.test(breakpoint)) {
         setTimeout(() => {
-          setOutput((prevOutput) => `${prevOutput}---`);
+          setOutput((prevOutput) => {
+            const newOutput = [...prevOutput];
+            const prevLineOutput = prevOutput[currentLineIndex] || '';
+            newOutput[currentLineIndex] = `${prevLineOutput}---`;
+            return newOutput;
+          });
         }, breakpoint.split('-')[1] * 100);
       } else {
         setTimeout(() => {
           setOutput((prevOutput) => {
-            const newOutput = text.substr(0, prevOutput.length + 1);
+            const newOutput = [...prevOutput];
+            const prevLineOutput = prevOutput[currentLineIndex] || '';
+            newOutput[currentLineIndex] = lineText.substr(
+              0, prevLineOutput.length + 1,
+            );
             return newOutput;
           });
         }, 60);
       }
     }
-  }, [output]);
+  }, [output, currentLineIndex]);
 
   // Blink cursor
   useEffect(() => {
@@ -40,17 +70,25 @@ const StaticTypewriter = ({ text }) => {
   }, []);
 
   return (
-    <h1 className={styles.Typewriter}>
-      <div className={styles.Terminal}>
+    <div className={styles.Container}>
+      {output.map((lineOutput, index) => (
         <div>
-          <span>&gt;</span>
+          <h1 className={styles.Typewriter}>
+            <div className={`${styles.Terminal} ${index !== currentLineIndex ? styles.Faded : ''}`}>
+              <div>
+                <span>&gt;</span>
+              </div>
+              <div>
+                <div key={`tw-output-l${index}`}>
+                  {`${lineOutput.replace(BREAKPOINT_PATTERN, '')}`}
+                  <Cursor visible={showCursor && index === currentLineIndex} />
+                </div>
+              </div>
+            </div>
+          </h1>
         </div>
-        <div>
-          {`${output.replace(BREAKPOINT_PATTERN, '')}`}
-          <Cursor visible={showCursor} />
-        </div>
-      </div>
-    </h1>
+      ))}
+    </div>
   );
 };
 
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -3,10 +3,23 @@ import Container from '../../components/Container/Container';
 import StaticTypewriter from '../../components/Typewriter/StaticTypewriter';
 import styles from './LandingPage.module.css';
 
-const LandingPage = () => (
-  <Container boundless center className={styles.LandingPage}>
-    <StaticTypewriter text="Welcome to my open resume.-9- Press any key to continue..." />
-  </Container>
-);
+const LandingPage = () => {
+  const lines = [
+    'Welcome to my open resume.-9- Press the enter key to continue...',
+    'Name: Morgan Wowk',
+    'Current position: Software Developer',
+    'Company: Bold Commerce',
+    'Active languages: PHP, Golang, Javascript',
+    'Years of experience: 3+ professional, 8+ independent',
+    'Topics of interest: Software architecture, API design, enterprise development',
+    'Core values: Culture, social impact, innovation, growth, community',
+  ];
+
+  return (
+    <Container center className={styles.LandingPage}>
+      <StaticTypewriter lines={lines} />
+    </Container>
+  );
+};
 
 export default LandingPage;
